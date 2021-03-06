9c2e937a958d9b1d34ca921487c7f3d5357ad01f
Tobias King <tk.king.tk@gmail.com>
2021-03-06 13:52:09 +0100

    Fixed bug

--- bin/index.js
+++ bin/index.js
@@ -6,7 +6,7 @@ const util = require('util');
 readdir = util.promisify(fs.readdir)
 const path = require('path');
 const ERD = require('../lib/ERD');
-const allowedFormats=["svg", "dot", "xdot", "plain", "plain-ext", "ps", "ps2", "json", "json0"];
+const allowedFormats = ["svg", "dot", "xdot", "plain", "plain-ext", "ps", "ps2", "json", "json0"];
 const main = async () => {
   try {
     program
@@ -15,34 +15,33 @@ const main = async () => {
       .option('-o, --output <path>', 'set output path')
       .option('-f, --format [svg,dot,xdot,plain,plan-ext,ps,ps2,json,json0]')
       .option('-c, --color <color>')
-      .option('-i, --ignore-index','ignore any files called index.js')
+      .option('-i, --ignore-index', 'ignore any files called index.js')
       .parse(process.argv);
-    if(allowedFormats.indexOf(program.format)==-1){
-        console.log(`Format :'${program.format}', is not supported.`);
-        return;
+    if (allowedFormats.indexOf(program.format) == -1) {
+      console.log(`Format :'${program.format}', is not supported.`);
+      return;
     }
     if (program.path && program.output) {
-
       const modelDirectory = path.resolve(program.path);
       const outputFilePath = program.output;
       const modelsPath = await readdir(modelDirectory);
       const models = [];
       for (const _model of modelsPath) {
-        if (_model.indexOf('.js') != -1 && !(_model === "index.js" && program.ignoreIndex)){
-          const model = require(path.join(modelDirectory, _model));
+        if (_model.indexOf('.js') != -1 && !(_model === "index.js" && program.ignoreIndex)) {
+          const model = require(path.join(modelDirectory, _model)).model;
           models.push(model);
         }
       }
       const svg = await ERD.generateFromModels(models, {
-        format:program.format,
+        format: program.format,
         collection: {
-          nameColor:'lightblue',
-          backgroundColor: program.color || '#4477c9'
+          nameColor: 'lightblue',
+          backgroundColor: program.color || '#4477c9',
         }
       });
 
       fs.writeFileSync(outputFilePath, svg);
-      console.log('ERD written to',outputFilePath);
+      console.log('ERD written to', outputFilePath);
     }
   } catch (e) {
     console.log(e)
