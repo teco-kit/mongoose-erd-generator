b65640246febd71201e6f3181c55cb797a91fe89
Tobias King <tk.king.tk@gmail.com>
2021-06-13 15:48:47 +0200

    Converted library to Koa-Middleware

--- bin/index.js
+++ bin/index.js
@@ -1,52 +1,51 @@
 #!/usr/bin/env node
-var program = require('commander');
-const process = require('process');
-const fs = require('fs');
-const util = require('util');
-readdir = util.promisify(fs.readdir)
-const path = require('path');
-const ERD = require('../lib/ERD');
-const allowedFormats = ["svg", "dot", "xdot", "plain", "plain-ext", "ps", "ps2", "json", "json0"];
-const main = async () => {
-  try {
-    program
-      .version('0.1.0')
-      .option('-p, --path <path>', 'set models path wanted to generate an ERD from.')
-      .option('-o, --output <path>', 'set output path')
-      .option('-f, --format [svg,dot,xdot,plain,plan-ext,ps,ps2,json,json0]')
-      .option('-c, --color <color>')
-      .option('-i, --ignore-index', 'ignore any files called index.js')
-      .parse(process.argv);
-    if (allowedFormats.indexOf(program.format) == -1) {
-      console.log(`Format :'${program.format}', is not supported.`);
-      return;
-    }
-    if (program.path && program.output) {
-      const modelDirectory = path.resolve(program.path);
-      const outputFilePath = program.output;
-      const modelsPath = await readdir(modelDirectory);
-      const models = [];
-      for (const _model of modelsPath) {
-        if (_model.indexOf('.js') != -1 && !(_model === "index.js" && program.ignoreIndex)) {
-          const model = require(path.join(modelDirectory, _model)).model;
-          models.push(model);
-        }
-      }
-      const svg = await ERD.generateFromModels(models, {
-        format: program.format,
-        collection: {
-          nameColor: 'lightblue',
-          backgroundColor: program.color || '#4477c9',
-        }
-      });
+var program = require("commander");
+const fs = require("fs");
+const util = require("util");
+readdir = util.promisify(fs.readdir);
+const path = require("path");
+const ERD = require("../lib/ERD");
 
-      fs.writeFileSync(outputFilePath, svg);
-      console.log('ERD written to', outputFilePath);
+
+generateSvg = async (modelDirectory, backgroundColor, nameColor) => {
+  const modelsPath = await readdir(modelDirectory);
+  const models = [];
+  for (const _model of modelsPath) {
+    if (
+      _model.indexOf(".js") != -1 &&
+      !(_model === "index.js" && program.ignoreIndex)
+    ) {
+      const model = require(path.join(modelDirectory, _model)).model;
+      models.push(model);
     }
-  } catch (e) {
-    console.log(e)
-    throw e;
   }
-}
-//////////////////////
-main();
+  const svg = await ERD.generateFromModels(models, {
+    format: "svg",
+    collection: {
+      nameColor: nameColor || "#ffffff",
+      backgroundColor: backgroundColor || "#ffffff",
+    },
+  });
+  return svg;
+};
+
+module.exports.dbSchema = (servePath, schemaSettings, templatePath) => {
+  const templateHTML = fs.readFileSync(templatePath, 'utf8');
+  var html = undefined;
+  var svg = undefined;
+  (async () => {
+    svg = await generateSvg(schemaSettings.modelsPath, schemaSettings.backgroundColor, schemaSettings.nameColor);
+    svg = svg.replace(new RegExp(/width=\"\d*..\"/gm), "")
+    svg = svg.replace(new RegExp(/height=\"\d*..\"/gm), "")
+    html = templateHTML.replace('fakeDbSchema', svg)
+  })();
+  return async (ctx, next) => {
+    if (ctx.path === servePath && ctx.method == "GET" && ctx.method != "Head") {
+      ctx.type = "text/html";
+      ctx.body = html;
+      return ctx;
+    } else {
+      return next();
+    }
+  };
+};
